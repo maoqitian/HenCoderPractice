d174150e9cc33a1ea2301b5b89bbfe075fe3970f
maoqitian <maoqitian068@163.com>
2021-03-28 21:14:09 +0800

    update project

--- MotionLayoutProject/app/src/main/java/com/mao/motionlayoutproject/MainActivity.kt
+++ MotionLayoutProject/app/src/main/java/com/mao/motionlayoutproject/MainActivity.kt
@@ -2,10 +2,17 @@ package com.mao.motionlayoutproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.constraintlayout.motion.widget.MotionLayout.DEBUG_SHOW_PATH
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.showPath).setOnClickListener {
+            findViewById<MotionLayout>(R.id.motionLayout).setDebugMode(DEBUG_SHOW_PATH)
+        }
     }
 }
