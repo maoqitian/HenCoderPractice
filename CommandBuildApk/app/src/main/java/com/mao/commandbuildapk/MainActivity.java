6ecf8ab33aa7bed7f48fe9548a10aa6a17cd9c30
maoqitian <maoqitian068@163.com>
2021-04-04 23:27:51 +0800

    add command build apk project

---
+++ CommandBuildApk/app/src/main/java/com/mao/commandbuildapk/MainActivity.java
@@ -0,0 +1,18 @@
+package com.mao.commandbuildapk;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * author : maoqitian
+ * date : 2021/4/4 17:59
+ * description :
+ */
+class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
