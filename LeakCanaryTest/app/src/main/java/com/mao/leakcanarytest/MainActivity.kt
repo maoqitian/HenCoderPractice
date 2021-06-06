a00fe3ed537804b22be02229f82e008d4286c639
maoqitian <maoqitian068@163.com>
2021-06-06 22:58:10 +0800

    update leakcanary project

--- LeakCanaryTest/app/src/main/java/com/mao/leakcanarytest/MainActivity.kt
+++ LeakCanaryTest/app/src/main/java/com/mao/leakcanarytest/MainActivity.kt
@@ -2,12 +2,16 @@ package com.mao.leakcanarytest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import android.os.Trace
 import android.util.Log
 import com.mao.leakcanarytest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Trace.beginSection("onCreate")
         //ViewBinding 方式绑定 view
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -18,6 +22,8 @@ class MainActivity : AppCompatActivity() {
             LeakTest(this).leak()
             finish()
         }
+        SystemClock.sleep(3000)
+        Trace.endSection()
 
     }
 
