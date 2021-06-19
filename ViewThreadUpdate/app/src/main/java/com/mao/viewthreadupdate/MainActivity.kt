e744e00ab0a30a8419140d94a85e0bd6babccdf9
maoqitian <maoqitian068@163.com>
2021-06-19 16:05:38 +0800

    update view thread project

--- ViewThreadUpdate/app/src/main/java/com/mao/viewthreadupdate/MainActivity.kt
+++ ViewThreadUpdate/app/src/main/java/com/mao/viewthreadupdate/MainActivity.kt
@@ -17,7 +17,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        /**
+         * 方法零 直接在onCreate 方法中更新 UI 目前 ViewRootImpl 还没创建，也就没有线程检查
+         */
 
+       /* thread {
+            tvText.text = "${Thread.currentThread()},${SystemClock.uptimeMillis()}"
+        }*/
         /**
          * 子线程更新UI
          */
@@ -55,21 +61,21 @@ class MainActivity : AppCompatActivity() {
          * 将 TextView 设置固定宽高 在 setText 方法中判断支持硬件加速绘制方法，而跳过了 requestLayout 方法调用，
          * 然后调用 invalidate方法，该方法没有检查线程判断
          * 如果去除硬件加速则会报错
-         * android:hardwareAccelerated="false"
+         * android:hardwareAccelerated="false" (默认开启硬件加速)
          * 自定义 View 设置了大小也同时开启硬件加速也同样使用子线程更新
          */
-        tvText.setOnClickListener {
+        /*tvText.setOnClickListener {
             //直接子线程更新
             thread {
                 tvText.text = "${Thread.currentThread()},${SystemClock.uptimeMillis()}"
             }
-        }
+        }*/
         /**
          * 方法四
          * 使用 surfaceView 来绘制
          * SurfaceView 支持子线程更新
          */
-        surfaceview.holder.addCallback(object :SurfaceHolder.Callback{
+        /*surfaceview.holder.addCallback(object :SurfaceHolder.Callback{
             override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
             }
 
@@ -93,7 +99,19 @@ class MainActivity : AppCompatActivity() {
 
             }
 
-        })
+        })*/
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+        /**
+         * 控件如果设置宽高并支持硬件加速同理也能支持子线程刷新
+         * 在 onResume 方法回调之后延迟等待 ViewRootImpl 创建好之后再更新 UI 就会触发线程检查
+         */
+        thread {
+            SystemClock.sleep(2000)
+            tvText.text = "${Thread.currentThread()},${SystemClock.uptimeMillis()}"
+        }
     }
 }
