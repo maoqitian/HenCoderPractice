11a80d08bae44aeeb432186a891a4c73e8e44712
mao <maoqitian068@163.com>
2021-07-16 19:06:11 +0800

    update code

--- Kotlin/app/src/main/java/com/example/app/test.kt
+++ Kotlin/app/src/main/java/com/example/app/test.kt
@@ -1,8 +1,14 @@
 package com.example.app
 
 import com.example.app.entity.User
+import com.example.core.retrofit.Repo
 import com.example.core.retrofit.RetrofitApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 /**
  * @Description:
@@ -16,11 +22,23 @@ fun main(){
     println(user == copy)
     println(user === copy)
 
-    val retrofit = Retrofit.Builder().baseUrl("https://api.github.com/").build()
+    val retrofit = Retrofit.Builder()
+        .baseUrl("https://api.github.com/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+        .build()
     val api = retrofit.create(RetrofitApi::class.java)
 
     val listRepos = api.listRepos("maoqitian")
 
-    listRepos.enqueue()
+    listRepos.enqueue(object :Callback<List<Repo>>{
+        override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
+
+        }
+
+        override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
+        }
+
+    })
     listRepos.execute()
 }
